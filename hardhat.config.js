d8e4f3af99546a8dbf4205d4c81c97284377287e
Teddy Jean-Francois <teddy.jean-francois@ibm.com>
2022-06-02 11:41:11 +0200

    fix : remove etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,8 +13,5 @@ module.exports = {
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${ process.env.ALCHEMY_API_KEY}`
     }
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
