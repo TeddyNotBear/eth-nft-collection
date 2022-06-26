27cf94b411a4c4e5ba8239b20fc8e40125830a5d
Jacky LIM <j4cky.lm@gmail.com>
2022-06-26 23:59:16 +0200

    feat: update breeding contract dependencies

--- contracts/Breeding.sol
+++ contracts/Breeding.sol
@@ -2,20 +2,23 @@
 pragma solidity ^0.8.4;
 
 import "./NftCollection.sol";
+import "./BreededNft.sol";
 
 contract Breeding {
     NftCollection public immutable nftCollection;
+    BreededNft public immutable breededNft;
 
-    constructor(NftCollection _nftCollection) {
+    constructor(NftCollection _nftCollection, BreededNft _breededNft) {
         nftCollection = _nftCollection;
+        breededNft = _breededNft;
     }
 
     function breed(uint256 token1, uint256 token2) public payable {
 
         require(nftCollection.ownerOf(token1) == msg.sender, "It's not your NFT");
         require(nftCollection.ownerOf(token2) == msg.sender, "It's not your NFT");
-
-        nftCollection.airdropNfts{value: msg.value}(msg.sender, 1);
+        
+        breededNft.mintNft{value: msg.value}(1);
 
         nftCollection.burnNft(token1);
         nftCollection.burnNft(token2);
