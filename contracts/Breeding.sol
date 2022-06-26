259426b496bf558b8213e7ca52dbd7da6c4c899e
Jacky LIM <j4cky.lm@gmail.com>
2022-06-26 19:12:18 +0200

    fix: breeding contract

--- contracts/Breeding.sol
+++ contracts/Breeding.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./NFTCollectionV2.sol";
+import "./NftCollectionV2.sol";
 
 contract Breeding {
     NftCollectionV2 public immutable nftCollectionV2;
@@ -12,12 +12,12 @@ contract Breeding {
 
     function breed(uint256 token1, uint256 token2) external {
 
-        require(nftCollectionV2.owner(token1) == msg.sender, "It's not your NFT");
-        require(nftCollectionV2.owner(token2) == msg.sender, "It's not your NFT");
+        require(nftCollectionV2.ownerOf(token1) == msg.sender, "It's not your NFT");
+        require(nftCollectionV2.ownerOf(token2) == msg.sender, "It's not your NFT");
 
         nftCollectionV2.airdropNfts(msg.sender, 1);
 
-        nftCollectionV2.burn(token1);
-        nftCollectionV2.burn(ken2);
+        nftCollectionV2.burnNft(token1);
+        nftCollectionV2.burnNft(token2);
     }
 }
