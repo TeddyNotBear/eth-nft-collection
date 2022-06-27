84167895068ca70ce4be3a34eff522e80ee89b9c
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2022-06-27 02:02:49 +0200

    modif HybridToken

--- contracts/HybridToken.sol
+++ contracts/HybridToken.sol
@@ -10,6 +10,9 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract HybridToken is ERC1155, Ownable, ReentrancyGuard, Pausable {
+
+    mapping(address => bool) admins;
+
     using Counters for Counters.Counter;
     using Strings for uint256;
 
@@ -17,12 +20,18 @@ contract HybridToken is ERC1155, Ownable, ReentrancyGuard, Pausable {
 
     uint256 public constant REWARD_TOKENS = 0;
 
-    constructor() public ERC1155("") {
-        _mint(msg.sender, REWARD_TOKENS, 10**18, "");
+    constructor() public ERC1155("Test") {}
+
+    function mint(address _to, uint256 id, uint _amount, bytes memory data) external {
+        require(admins[msg.sender], "not able to mint");
+        _mint(_to, id, _amount, data);
     }
 
-    function breeding() external {
-
+    function addAdmin(address _admin) external onlyOwner {
+        admins[_admin] = true;
     }
 
+    function removeAdmin(address _admin) external onlyOwner {
+        admins[_admin] = false;
+    }
 }
